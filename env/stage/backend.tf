f20bdd44ec59d6132970d56b5f114f5a91fc5f8c
asheesh1303-netizen <asheesh1303@gmail.com>
2026-03-23 13:21:48 +0000

    Initial terraform project

---
+++ env/stage/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-asheesh-123"
+    key            = "stage/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
